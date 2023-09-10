f412a3034d53560d38d0eb569a2fe37dc7e33810
Damian Sire <damiansire@hotmail.com>
2023-09-10 17:11:16 -0300

    feat: add base for generated routes

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,73 @@
-console.log("hello")
+import * as fs from 'fs';
+import * as path from 'path';
+
+class FilesPathManger {
+    paths: Map<string, string[]>;
+    constructor() {
+        this.paths = new Map();
+    }
+    addPath(base: string, path: string) {
+        if (!this.paths.has(base)) {
+            this.paths.set(base, []);
+        }
+        const pathsArray = this.paths.get(base);
+        if (pathsArray) {
+            pathsArray.push(path);
+        }
+    }
+    addMultiplesPaths(base: string, path: string[]) {
+        if (!this.paths.has(base)) {
+            this.paths.set(base, []);
+        }
+        const currentData = this.paths.get(base)
+        if (currentData) {
+            const newData = [...currentData, ...path];
+            this.paths.set(base, newData);
+        }
+    }
+    getKeys() {
+        return this.paths.keys();
+    }
+    getByKey(key: string) {
+        return this.paths.get(key)
+    }
+    mergeFilesPath(otherManager: FilesPathManger) {
+        for (const base of otherManager.getKeys()) {
+            const data = otherManager.getByKey(base);
+            if (data) {
+                this.addMultiplesPaths(base, data);
+            }
+        }
+    }
+}
+
+function readFilesRecursively(directoryPath: string) {
+    const filesPaths = new FilesPathManger();
+    const files = fs.readdirSync(directoryPath);
+
+    files.forEach((file) => {
+        const filePath = path.join(directoryPath, file);
+        const stats = fs.statSync(filePath);
+
+        if (stats.isDirectory()) {
+            // Si es un directorio, llama a la función recursivamente
+            const generatedFilePaths = readFilesRecursively(filePath);
+            filesPaths.mergeFilesPath(generatedFilePaths);
+        } else if (stats.isFile()) {
+            const fileRelativePath = filePath.replace(/^src\\test\\notas\\/, '');
+            if (!fileRelativePath.endsWith("index.md")) {
+                const base = fileRelativePath.split(path.sep)[0];
+                filesPaths.addPath(base, filePath)
+            }
+        }
+    });
+
+    return filesPaths;
+}
+
+
+// Ruta al directorio que deseas leer
+const directorio = './src/test/notas';
+
+const result = readFilesRecursively(directorio);
+console.log(result)
